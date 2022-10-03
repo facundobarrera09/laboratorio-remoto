cb57785153ced0fdad93a01fc74545c5bbb3353d
Facundo Barrera <barreraaybarfacundo@gmail.com>
2022-10-03 15:58:54 -0300

    Añadir calculo de desfasaje

--- app/MeasurementData.js
+++ app/MeasurementData.js
@@ -3,6 +3,23 @@ class MeasurementData {
         this.size = (voltageArray.length > intensityArray.length) ? voltageArray.length : intensityArray.length;
         this.voltage = voltageArray;
         this.intensity = intensityArray;
+        this.phaseShift = this.calculatePhaseShift(voltageArray, intensityArray);
+    }
+
+    calculatePhaseShift(voltage, intensity) {
+        let phaseShift = [];
+
+        for (let k = 0; k < voltage.length; k++) {
+            let sum = [];
+
+            for (let i = 0; i < (voltage.length-1-k); i++) {
+                sum[i] = intensity[k+i] * voltage[i];
+            }
+
+            phaseShift[k] = sum.reduce((prev, curr) => prev + curr, 0);
+        }
+
+        return phaseShift;
     }
 
     get getSize() {
