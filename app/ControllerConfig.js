663e8e76257be2616552180b13c8765ba2f1a870
Facundo Barrera <barreraaybarfacundo@gmail.com>
2022-11-22 20:44:59 -0300

    Agregar reles faltantes

--- app/ControllerConfig.js
+++ app/ControllerConfig.js
@@ -6,6 +6,9 @@ class ControllerConfig {
     updateConfig(config) {
         this.rele1 = config['rele1'];
         this.rele2 = config['rele2'];
+        this.rele3 = config['rele3'];
+        this.rele4 = config['rele4'];
+        this.rele5 = config['rele5'];
     }
 };
 
